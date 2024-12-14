61be080f3257c7ecfe696f34ffb7a647f567a75e
PykitYT <broiwillbanyou@gmail.com>
2024-12-14 12:44:18 +0200

    Сделана библотека для замена текста.

---
+++ trno.py
@@ -0,0 +1,8 @@
+#trno.py
+class Trno:
+    def __init__(self):
+        pass
+    def trn(self, tr={"python": "thonny"}, text="python is cool!(:"):
+        for a, b in tr.items():
+            text = text.replace(a, b)
+        return text
